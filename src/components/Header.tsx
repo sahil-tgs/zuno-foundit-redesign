cc8d06a486a9c4dda18e3b9041e14628a5dd666c
sahil-tgs <sahiltgs@gmail.com>
2024-04-28 11:42:06 +0530

    header prototype

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,39 @@
+// Header.tsx
 import React from 'react';
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import { AppBar, Toolbar, Button, Box, IconButton } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import logo from '../../public/logo.svg';
+
+const buttonStyle = {
+  color: 'black',
+  fontWeight: 'bold', // Added bold font weight for buttons
+};
 
 const Header: React.FC = () => {
   return (
-    <AppBar position="static">
+    <AppBar position="static" style={{ background: 'transparent', boxShadow: 'none' }}>
       <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          foundit
-        </Typography>
-        <Button color="inherit">Login</Button>
-        <Button color="inherit">Register</Button>
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <IconButton
+            size="large"
+            edge="start"
+            aria-label="menu"
+            sx={{ mr: 2, color: 'black' }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <img src={logo} alt="Foundit Logo" style={{ height: '20px' }} />
+        </Box>
+        <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center' }}>
+          <Button color="inherit" style={buttonStyle}>Career Fair</Button>
+          <Button color="inherit" style={buttonStyle}>Skill Tests</Button>
+          <Button color="inherit" style={buttonStyle}>Premium Services</Button>
+          <Button color="inherit" style={buttonStyle}>Resume Builder</Button>
+        </Box>
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <Button color="inherit" style={buttonStyle}>Login</Button>
+          <Button color="inherit" style={buttonStyle}>Register</Button>
+        </Box>
       </Toolbar>
     </AppBar>
   );
