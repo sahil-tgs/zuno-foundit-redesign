983893587180992e38af48d73347b3c74f2b6c15
sahil-tgs <sahiltgs@gmail.com>
2024-12-16 10:08:11 +0530

    test-build

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Container, Box } from '@mui/material';
-import Header from './components/Header';
-import SearchSection from './components/SearchSection';
+// import { Container, Box } from '@mui/material';
+// import Header from './components/Header';
+// import SearchSection from './components/SearchSection';
 import HeroSection from './components/HeroSection';
-import GetStartedSection from './components/GetStartedSection';
-import EmployersSection from './components/EmployersSection';
-import PopularSearches from './components/PopularSearches';
-import FeaturedSection from './components/FeaturedSection';
+// import GetStartedSection from './components/GetStartedSection';
+// import EmployersSection from './components/EmployersSection';
+// import PopularSearches from './components/PopularSearches';
+// import FeaturedSection from './components/FeaturedSection';
 import Footer from './components/Footer';
 import './App.css'; // Import the App.css file
 
