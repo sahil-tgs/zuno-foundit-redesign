2d2c12b9e05f95b953ad2175507371f5b31b8a28
sahil-tgs <sahiltgs@gmail.com>
2024-12-16 10:10:48 +0530

    Box not defined- Deployment fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Box from '@mui/material';
 // import { Container, Box } from '@mui/material';
 // import Header from './components/Header';
 // import SearchSection from './components/SearchSection';
